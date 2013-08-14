2112d475bd2b14e59f6749f2c521490b00767199
Michael Clayton <mostly_mike@hotmail.com>
2013-08-14 19:04:46 -0400

    Added all work and resources I have created thus far.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-online-portfolio
+My Online Portfolio
 ================
 
-My Online Portfolio
+This my website where I can put works I have created as a software engineer.
---
+++ functionality.js
@@ -0,0 +1,42 @@
+var optionSelected = false;
+
+function sendEmail() {
+  var name = $("#contactName").val();
+  var email = $("#contactEmail").val();
+  var message = $("#contactMessage").val();
+  var link = "mailto:mostly_mike@hotmail.com"
+             + "?subject=" + encodeURIComponent("Contact Form Response")
+             + "&body=" + encodeURIComponent(message+"\n\nFrom:\n"+name+"\n"+email);
+  window.location.href = link;
+}
+
+function setSpeechBubbleText() {
+   if (!optionSelected) {
+   var speechValue = $("#speechValue").val();
+   
+   if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+     if (speechValue === "resume")
+       $("#speechBubbleText").html("Thanks for viewing my resume!");
+    else if (speechValue === "contact")
+       $("#speechBubbleText").html("I'd love to hear from you!");
+    else if (speechValue === "about")
+       $("#speechBubbleText").html("Enjoying your visit so far?<br><a class=\"btn btn-success btn-small\" onclick=\"selectYes()\">Yes</a><a class=\"btn btn-danger btn-small\" onclick=\"selectNo()\">No</a>");
+   } else {
+     $("#speechBubbleText").html("...<br>");
+   }
+   }
+}
+
+function selectYes() {
+  optionSelected = true;
+  $("#speechBubbleText").html("That's Fantastic! I hope you<br>find the remainder enjoyable as well!");
+}
+
+function selectNo() {
+  optionSelected = true;
+  $("#speechBubbleText").html("Sorry to hear that. Please let me know why!<br><a href=\"contact.html\" class=\"btn btn-info btn-small\">Contact</a>");
+}
+
+$(document).scroll(function(e) {
+  setSpeechBubbleText();
+});
