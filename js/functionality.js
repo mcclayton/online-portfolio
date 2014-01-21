8f7b1b643f798341e601568640c191d2ef1dc9d3
Michael Clayton <mostly_mike@hotmail.com>
2014-01-21 14:59:11 -0500

    Added cool terminal title to main page.

--- js/functionality.js
+++ js/functionality.js
@@ -9,3 +9,31 @@ function sendEmail() {
              + "&body=" + encodeURIComponent(message+"\n\nFrom:\n"+name+"\n"+email);
   window.location.href = link;
 }
+
+
+
+
+var currentCount = 0;
+
+var blinkTitle = function() {
+    var mainTitle = $('#mainTitle');
+    
+    currentCount = (currentCount+1)%2;
+    if (currentCount == 0) {
+        mainTitle.html("<FONT COLOR=\"f37b1d\">~</FONT><FONT COLOR=\"00f943\">$</FONT> Michael Clayton<FONT COLOR=\"ffffff\"> |</FONT>");
+    } else {
+        mainTitle.html("<FONT COLOR=\"f37b1d\">~</FONT><FONT COLOR=\"00f943\">$</FONT> Michael Clayton<FONT COLOR=\"00f943\"> |</FONT>");
+    }
+};
+
+// setTimeout Example
+var loopTitleBlink = function() {
+    blinkTitle();
+    setTimeout(loopTitleBlink, 700);
+};
+
+
+window.onload = function(){
+    $('#mainTitle').html("HI");
+    loopTitleBlink();
+};
