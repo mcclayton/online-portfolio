d1468c8c10e674bef86c9b8a62b703156bea9e80
Michael Clayton <mostly_mike@hotmail.com>
2014-02-28 02:45:44 -0500

    Added images of me to about me page.

---
+++ js/hover.js
@@ -0,0 +1,7 @@
+function mouseOverImage(image_id, imagePath) {
+    document.getElementById(image_id).src = imagePath;
+}
+ 
+function mouseOutImage(image_id, imagePath) {
+    document.getElementById(image_id).src = imagePath;
+}
