a3de096d42bdc8021706c2da1bbc434c4e9951f8
Michael Clayton <michael.clayton@procore.com>
2020-10-23 13:12:22 -0700

    Move footer to component with scss module styles

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PT from 'prop-types';
+import rocket from '../../images/rocket_emoji.png';
+import styles from './styles.module.scss';
+
+const Footer = ({ timeout }) => (
+  <footer id="footer" style={timeout ? { display: 'none' } : {}}>
+    <ul className="icons">
+      <li>
+        <a href="https://www.linkedin.com/in/michaelcclayton/" className="icon fa-linkedin">
+          <span className="label">LinkedIn</span>
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/mcclayton" className="icon fa-github">
+          <span className="label">GitHub</span>
+        </a>
+      </li>
+    </ul>
+    <div className={styles.sourceCodeContainer}>
+      View This Site's <a href="https://github.com/mcclayton/mcclayton.github.io">Source Code</a>
+      <img className={styles.emojiImage} src={rocket} alt="Rocket Emoji" />
+    </div>
+  </footer>
+);
+
+Footer.propTypes = {
+  timeout: PT.bool
+};
+
+export default Footer;
