01efec4bba275802a4719d2d6615d62c78b657ed
Michael Clayton <michael.clayton@procore.com>
2020-10-24 11:12:19 -0700

    Handle escape key for closing articles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
-import React from 'react'
-import Layout from '../components/layout'
+import React from 'react';
+import Layout from '../components/layout';
 
-import Header from '../components/Header'
-import Main from '../components/Main'
-import Footer from '../components/Footer'
+import Header from '../components/Header';
+import Main from '../components/Main';
+import Footer from '../components/Footer';
+
+const ESCAPE_KEY_VALUES = [27, 'Escape'];
 
 class IndexPage extends React.Component {
   constructor(props) {
@@ -15,72 +17,95 @@ class IndexPage extends React.Component {
       article: '',
       loading: 'is-loading',
     }
-    this.handleOpenArticle = this.handleOpenArticle.bind(this)
-    this.handleCloseArticle = this.handleCloseArticle.bind(this)
-    this.setWrapperRef = this.setWrapperRef.bind(this)
-    this.handleClickOutside = this.handleClickOutside.bind(this)
+    this.handleOpenArticle = this.handleOpenArticle.bind(this);
+    this.handleCloseArticle = this.handleCloseArticle.bind(this);
+    this.setWrapperRef = this.setWrapperRef.bind(this);
+    this.handleClickOutside = this.handleClickOutside.bind(this);
+    this.handleEscapeKey = this.handleEscapeKey.bind(this);
   }
 
   componentDidMount() {
     this.timeoutId = setTimeout(() => {
       this.setState({ loading: '' })
     }, 100)
+    document.addEventListener('keyup', this.handleEscapeKey);
+
     document.addEventListener('mousedown', this.handleClickOutside)
+  }
+
+  handleEscapeKey(e) {
+    debugger;
+    function getKeyCode(event) {
+      if (event.key !== undefined) {
+        return event.key;
+      } else if (event.keyIdentifier !== undefined) {
+        return event.keyIdentifier;
+      } else if (event.keyCode !== undefined) {
+        return event.keyCode;
+      }
+    };
+
+    if (ESCAPE_KEY_VALUES.includes(getKeyCode(e))) {
+      if (this.state.isArticleVisible) {
+        this.handleCloseArticle();
+      }
+    }
   }
 
   componentWillUnmount() {
     if (this.timeoutId) {
-      clearTimeout(this.timeoutId)
+      clearTimeout(this.timeoutId);
     }
-    document.removeEventListener('mousedown', this.handleClickOutside)
+    document.removeEventListener('keyup', this.handleEscapeKey);
+    document.removeEventListener('mousedown', this.handleClickOutside);
   }
 
   setWrapperRef(node) {
-    this.wrapperRef = node
+    this.wrapperRef = node;
   }
 
   handleOpenArticle(article) {
     this.setState({
       isArticleVisible: !this.state.isArticleVisible,
       article,
-    })
+    });
 
     setTimeout(() => {
       this.setState({
         timeout: !this.state.timeout,
-      })
-    }, 325)
+      });
+    }, 325);
 
     setTimeout(() => {
       this.setState({
         articleTimeout: !this.state.articleTimeout,
-      })
-    }, 350)
+      });
+    }, 350);
   }
 
   handleCloseArticle() {
     this.setState({
       articleTimeout: !this.state.articleTimeout,
-    })
+    });
 
     setTimeout(() => {
       this.setState({
         timeout: !this.state.timeout,
-      })
-    }, 325)
+      });
+    }, 325);
 
     setTimeout(() => {
       this.setState({
         isArticleVisible: !this.state.isArticleVisible,
         article: '',
-      })
-    }, 350)
+      });
+    }, 350);
   }
 
   handleClickOutside(event) {
     if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
       if (this.state.isArticleVisible) {
-        this.handleCloseArticle()
+        this.handleCloseArticle();
       }
     }
   }
@@ -111,8 +136,8 @@ class IndexPage extends React.Component {
           <div id="bg" />
         </div>
       </Layout>
-    )
+    );
   }
 }
 
-export default IndexPage
+export default IndexPage;
