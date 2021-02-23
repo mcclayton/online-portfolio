1c688b65f6bca372376fa9f7cd7c99573abde9c4
Michael Clayton <michael.clayton@procore.com>
2021-02-23 11:12:06 -0800

    Begin working on contact form

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,6 +3,7 @@ import React, { useEffect, useCallback } from 'react';
 import { ARTICLE_IDS } from '../../constants';
 import Article from '../Article';
 import UnderConstruction from './UnderConstruction';
+import ContactForm from '../ContactForm';
 import CodeMemoji from '../CodeMemoji';
 import pic01 from '../../images/pic01.jpg';
 import wave from '../../images/wave_emoji.png';
@@ -88,28 +89,11 @@ function Main({
         id={ARTICLE_IDS.Contact}
         {...baseArticleProps}
       >
-        <form method="post" action="#">
-          <div className="field half first">
-            <label htmlFor="name">Name</label>
-            <input type="text" name="name" id="name" />
-          </div>
-          <div className="field half">
-            <label htmlFor="email">Email</label>
-            <input type="text" name="email" id="email" />
-          </div>
-          <div className="field">
-            <label htmlFor="message">Message</label>
-            <textarea name="message" id="message" rows="4"></textarea>
-          </div>
-          <ul className="actions">
-            <li>
-              <input type="submit" value="Send Message" className="special" />
-            </li>
-            <li>
-              <input type="reset" value="Reset" />
-            </li>
-          </ul>
-        </form>
+        <blockquote>
+          I seek opportunities to challenge my intellect, develop my professional career, and make an impact.
+          If you have such an opportunity, or are interested in working with me, please reach out to me using the form below.
+        </blockquote>
+        <ContactForm />
       </Article>
     </div>
   );
