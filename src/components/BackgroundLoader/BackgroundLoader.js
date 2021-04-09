5d01b70fae1a4abeba3f92c777a36feae1440b57
Michael Clayton <michael.clayton@procore.com>
2021-04-09 14:23:46 -0700

    Remove artificial delay

--- src/components/BackgroundLoader/BackgroundLoader.js
+++ src/components/BackgroundLoader/BackgroundLoader.js
@@ -2,19 +2,13 @@ import React, { useEffect, useState, useRef } from 'react';
 import Spinner from '../Spinner';
 import background from '../../images/bg.jpg';
 
-const MIN_DELAY = 200;
-
 function BackgroundLoader({ children }) {
   const imgRef = useRef(null);
   const [isLoading, setIsLoading] = useState(true);
 
-  function finishLoading() {
-    setTimeout(() => setIsLoading(false), Math.floor(Math.random() * (MIN_DELAY * 2)) + MIN_DELAY);
-  }
-
   useEffect(() => {
     if (imgRef.current?.complete) {
-      finishLoading();
+      setIsLoading(false);
     }
   }, []);
 
@@ -29,7 +23,7 @@ function BackgroundLoader({ children }) {
             style={{ display: 'none' }}
             ref={imgRef}
             src={background}
-            onLoad={finishLoading}
+            onLoad={setIsLoading(false)}
             decoding="async"
           />
         )
