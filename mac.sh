7990abfb8b45e113a347959673fdc441567fdbba
Michael Clayton <michael.clayton@procore.com>
2021-07-14 12:35:15 -0700

    Updates

--- mac.sh
+++ mac.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 SETUP_DIR=~/.mac_setup
 
+# Show a spinner until the last process (must be background process) finishes
+function spinner() {
+  spin='-\|/'
+  i=0
+  while kill -0 $pid 2>/dev/null
+  do
+    i=$(( (i+1) %4 ))
+    printf "\r${spin:$i:1} Preparing Mac Setup..."
+    sleep .1
+  done
+  echo -e "\n\n"
+}
+
 # Make setup directory
 mkdir -p $SETUP_DIR
 cd $SETUP_DIR
@@ -10,14 +23,7 @@ cd $SETUP_DIR
 pid=$! # Process Id of the previous running command
 
 # Show Spinner
-spin='-\|/'
-i=0
-while kill -0 $pid 2>/dev/null
-do
-  i=$(( (i+1) %4 ))
-  printf "\r${spin:$i:1} Preparing Mac Setup..."
-  sleep .1
-done
+spinner
 
 # Invoke installation script
-./install.sh
+./start.sh
